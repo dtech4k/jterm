40f8a7afe144096cce9eeeac518b5a330c78ad3c
dtech4k <134433902+dtech4k@users.noreply.github.com>
2024-01-24 14:43:11 -0500

    Add files via upload

---
+++ doisigners.js
@@ -0,0 +1,15 @@
+var signers = [
+    ["Georgia", [ "Button Gwinnett", "Lyman Hall", "George Walton" ]],
+    ["North Carolina", [ "William Hooper", "Joseph Hewes", "John Penn" ]],
+    ["South Carolina", [ "Edward Rutledge", "Thomas Heyward, Jr.", "Thomas Lynch, Jr.", "Arthur Middleton" ]],
+    ["Maryland", [ "Samuel Chase", "William Paca", "Thomas Stone", "Charles Carroll, of Carrollton" ]], 
+    ["Virginia", [ "George Wythe", "Richard Henry Lee", "Thomas Jefferson", "Benjamin Harrison", "Thomas Nelson, Jr.", "Francis Lightfoot Lee", "Carter Braxton" ]],
+    ["Pennsylvania", [ "Robert Morris", "Benjamin Rush", "Benjamin Franklin", "John Morton", "George Clymer", "James Smith", "George Taylor", "James Wilson", "George Ross" ]],
+    ["Delaware", [ "Caesar Rodney", "George Read", "Thomas McKean" ]],
+    ["New York", [ "William Floyd", "Philip Livingston", "Francis Lewis", "Lewis Morris" ]],
+    ["New Jersey", [ "Richard Stockton", "John Witherspoon", "Francis Hopkinson", "John Hart", "Abraham Clark" ]],
+    ["New Hampshire", [ "Josiah Bartlett", "William Whipple", "Matthew Thornton" ]],
+    ["Massachusetts Bay", [ "Samuel Adams", "John Adams", "Robert Treat Paine", "Elbridge Gerry" ]],
+    ["Rhode Island and Providence, &c.", [ "Stephen Hopkins", "William Ellery" ]],
+    ["Connecticut", [ "Roger Sherman", "Samuel Huntington", "William Williams", "Oliver Wolcott" ]]
+]
