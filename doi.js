dc605401c06344de099f760b0f86e6f27c0a260e
dtech4k <134433902+dtech4k@users.noreply.github.com>
2024-01-25 14:19:17 -0500

    Add files via upload

--- doi.js
+++ doi.js
@@ -1,1 +1,25 @@
 var signerOpenTag = '<span class="signer">', signerCloseTag = '</span>', signer = 'Samuel Adams';
+
+function magnifyText(selection)
+{
+    var element = $(selection);
+    if (element.css('font-size') == '')
+        element.css('font-size', '120%');
+    else
+        element.css('font-size', parseInt(element.css('font-size')) * 1.20);
+}
+function shrinkText(selection)
+{
+    var element = $(selection);
+    if (element.css('font-size') == '')
+        element.css('font-size', '83%');
+    else
+        element.css('font-size', parseInt(element.css('font-size')) * 0.833);
+}
+
+function jhImageVisibility(element){
+    jhImage = $('#JohnHancockImage')
+    visibility = jhImage.css('visibility')
+    visibility = visibility == 'visible' ? 'hidden' : 'visible'
+    jhImage.css ('visibility', visibility)
+}
